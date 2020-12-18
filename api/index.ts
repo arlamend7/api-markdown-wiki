b5ac90712c74e5d223e78f2c0ff198012abb7aa0
arlan mendes <arlanmendes197@gmail.com>
2020-12-18 12:09:20 -0300

    vai

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 const app = express();
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin:"*", allowedHeaders:"*"}))
 app.use(
     body.urlencoded({
         extended : true
